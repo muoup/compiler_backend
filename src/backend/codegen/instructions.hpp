04f1043b2c196cba1f8cbb004b4393d7161c9487
Maste <48286570+Masterdust12@users.noreply.github.com>
2024-10-21 17:51:23 -0400

    added some type-safety to the codegen value system. instead of passing the raw pointers of the virtual memory structs directly, their aliases in the value map are passed. this prevents remapping a value from causing dangling pointers to be passed around.

--- src/backend/codegen/instructions.hpp
+++ src/backend/codegen/instructions.hpp
@@ -13,6 +13,8 @@ namespace ir::block {
 namespace backend::codegen {
     struct function_context;
 
+    using v_operands = std::vector<std::string>;
+
     struct instruction_return {
         std::unique_ptr<vptr> return_dest;
         bool valid = true;
@@ -21,41 +23,41 @@ namespace backend::codegen {
     instruction_return gen_allocate(
             backend::codegen::function_context &context,
             const ir::block::allocate &allocate,
-            std::vector<const vptr*> &virtual_operands
+            const v_operands &virtual_operands
     );
     instruction_return gen_store(
             backend::codegen::function_context &context,
             const ir::block::store &store,
-            std::vector<const vptr*> &virtual_operands
+            const v_operands &virtual_operands
     );
     instruction_return gen_load(
             backend::codegen::function_context &context,
             const ir::block::load &load,
-            std::vector<const vptr*> &virtual_operands
+            const v_operands &virtual_operands
     );
     instruction_return gen_icmp(
             backend::codegen::function_context &context,
             const ir::block::icmp &icmp,
-            std::vector<const vptr*> &virtual_operands
+            const v_operands &virtual_operands
     );
     instruction_return gen_branch(
             backend::codegen::function_context &context,
             const ir::block::branch &branch,
-            std::vector<const vptr*> &virtual_operands
+            const v_operands &virtual_operands
     );
     instruction_return gen_return(
             backend::codegen::function_context &context,
             const ir::block::ret &ret,
-            std::vector<const vptr*> &virtual_operands
+            const v_operands &virtual_operands
     );
     instruction_return gen_arithmetic(
             backend::codegen::function_context &context,
             const ir::block::arithmetic &arithmetic,
-            std::vector<const vptr*> &virtual_operands
+            const v_operands &virtual_operands
     );
     instruction_return gen_call(
             backend::codegen::function_context &context,
             const ir::block::call &call,
-            std::vector<const vptr*> &virtual_operands
+            const v_operands &virtual_operands
     );
 }
