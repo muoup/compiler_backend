598ed877126addbf6265c012c78dfed23e0fad35
muoup <masterdust12@gmail.com>
2024-11-04 15:36:07 -0500

    i'm not totally certain how to get this to be importable as a library, so my next few commits will probably be a lot of tweaking

--- library.h
+++ library.h
@@ -1,26 +1,31 @@
 #include "src/ir/nodes.hpp"
 
-struct function_builder;
+namespace cb {
+    struct function_builder;
 
-struct code_unit {
-    ir::root root;
+    struct code_unit {
+        ir::root root;
 
-    code_unit() = default;
+        code_unit() = default;
 
-    function_builder create_function(std::string name, ir::value_size return_type, std::vector<ir::variable> parameters);
-};
+        function_builder
+        create_function(std::string name, ir::value_size return_type, std::vector<ir::variable> parameters);
+    };
 
-struct function_builder {
-    code_unit *unit;
-    ir::global::function *current_function;
-    ir::block::block *current_block;
+    struct function_builder {
+        code_unit *unit;
+        ir::global::function *current_function;
+        ir::block::block *current_block;
 
-    void create_block(const std::string &name);
-    void checkout_last_block();
-    void checkout_block(const std::string &name);
+        void create_block(const std::string &name);
 
-    template <typename T, typename... Args>
-    void create_instruction(Args&&... args) {
-        current_block->instructions.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
-    }
-};
+        void checkout_last_block();
+
+        void checkout_block(const std::string &name);
+
+        template<typename T, typename... Args>
+        void create_instruction(Args &&... args) {
+            current_block->instructions.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
+        }
+    };
+}
